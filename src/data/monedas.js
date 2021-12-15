038dfe2970a06ce94273eacb4b059ecb8c6226e2
Robert Smart <roca1388@gmail.com>
2021-12-15 14:17:13 -0300

    Actualización de Monedas

--- src/data/monedas.js
+++ src/data/monedas.js
@@ -1,8 +1,11 @@
 const monedas = [
     { id: 'USD', nombre: 'Dolar de Estados Unidos'},
-    { id: 'MXN', nombre: 'Peso Mexicano'},
     { id: 'EUR', nombre: 'Euro'},
     { id: 'GBP', nombre: 'Libra Esterlina'},
+    { id: 'MXN', nombre: 'Peso Mexicano'},
+    { id: 'ARS', nombre: 'Peso Argentino'},
+    { id: 'COP', nombre: 'Peso Colombiano'},
+    { id: 'BRL', nombre: 'Real de Brasil'},
 ]
 
 export {
